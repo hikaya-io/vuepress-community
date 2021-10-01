d3123e0df4274a3ee0508e4ba4b38f47f051da5e
Anas <anastiour@gmail.com>
2021-10-01 23:32:33 +0100

    fix(clean-urls): do not remove trailling slash on root page

--- packages/vuepress-plugin-clean-urls/src/index.ts
+++ packages/vuepress-plugin-clean-urls/src/index.ts
@@ -23,7 +23,7 @@ const CleanUrlsPlugin: Plugin<CleanUrlsPluginOptions> = ({
       // normal path
       // e.g. foo/bar.md -> foo/bar.html
       page.path = regularPath.slice(0, -5) + normalSuffix
-    } else if (regularPath.endsWith('/')) {
+    } else if (regularPath.endsWith('/') && regularPath != '/') {
       // index path
       // e.g. foo/index.md -> foo/
       page.path = regularPath.slice(0, -1) + indexSuffix
